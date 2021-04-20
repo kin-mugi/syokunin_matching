8a00995ea329e964cb3895ceb7765c77b1e343e4
kin_mugi <hmfxp35143@yahoo.co.jp>
2021-04-20 23:20:01 +0900

    基本的なsession#new設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'profile' => 'users#profile'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
