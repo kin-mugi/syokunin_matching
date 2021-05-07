9f2fa15d86713b6138df989d61c585ce5993b365
kin_mugi <hmfxp35143@yahoo.co.jp>
2021-05-08 08:38:43 +0900

    migration実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_112311) do
+ActiveRecord::Schema.define(version: 2021_05_06_144418) do
 
   create_table "chats", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
