2601b7c1bd8d590d6e9b46df8329ede513c34102
kin_mugi <hmfxp35143@yahoo.co.jp>
2021-05-10 07:08:28 +0900

    Follow,Unfollow機能にajax実装準備

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+//jquery設定のために記述
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+
 module.exports = environment
